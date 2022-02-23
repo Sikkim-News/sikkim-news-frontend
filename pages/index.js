46e38c623b092a9d74e832a29cab8f3adff4b7ec
Ketan Mangla <Ketan_Mangla@Dell.com>
2022-02-23 20:58:08 +0530

    Set up folder structure final

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import styles from "@/styles/HomePage.module.css";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <div className={styles.HomePage}>
       <h1>Cheers To A New Beginning!</h1>
+      <Header />
     </div>
   );
 }
