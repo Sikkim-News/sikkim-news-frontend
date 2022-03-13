8557a75b3ca94d59f088056f938ca88d2818fd45
Ketan Mangla <Ketan_Mangla@Dell.com>
2022-03-13 23:18:09 +0530

    under construction final page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,61 @@
 import styles from "@/styles/HomePage.module.css";
-import Header from "@/components/Header";
+import {
+  FacebookRounded,
+  Telegram,
+  Twitter,
+  YouTube,
+  Instagram,
+} from "@mui/icons-material";
+import { IconButton } from "@mui/material";
+import Image from "next/image";
+
+const socialMediaLinks = [
+  {
+    icon: <FacebookRounded />,
+    url: "https://www.facebook.com/SikkimNews.NoFavourNoFear",
+  },
+  {
+    icon: <YouTube />,
+    url: "https://www.youtube.com/channel/UCjBaF2Ro5OlcKv-N5dwhx2A",
+  },
+  {
+    icon: <Twitter />,
+    url: "https://www.youtube.com/channel/UCjBaF2Ro5OlcKv-N5dwhx2A",
+  },
+  { icon: <Telegram />, url: "https://www.google.com" },
+  { icon: <Instagram />, url: "https://www.instagram.com" },
+];
 
 export default function Home() {
   return (
     <div className={styles.HomePage}>
-      <h1>Cheers To A New Beginning!</h1>
-      <Header />
+      <div className={styles.overlay}>
+        <div>
+          <Image
+            src="/resources/logo-dark.png"
+            height={100}
+            width={100}
+            // layout="fill"
+            alt="Logo Sikkim News"
+          />
+        </div>
+        Our Website is Under Construction
+        <div className={styles.socialmedia}>
+          <span>Follow us and stay updated!</span>
+          <div>
+            {socialMediaLinks.map((element, idx) => (
+              <IconButton
+                style={{ color: "#ae2012" }}
+                key={idx}
+                href={element.url}
+                target="_blank"
+              >
+                {element.icon}
+              </IconButton>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
